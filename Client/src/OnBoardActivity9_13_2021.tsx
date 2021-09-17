c315f952c108f0313ea7ef88af1dc9cdf6f1807b
JMantis0 <60026263+JMantis0@users.noreply.github.com>
2021-09-17 15:28:40 -0700

    Added javascript function LongestNonrepeating Substring and also changed a directory name

--- Client/src/OnBoardActivity9_13_2021.tsx
+++ Client/src/OnBoardActivity9_13_2021.tsx
@@ -12,34 +12,59 @@ const OnBoardActivity9_13_2021 = () => {
   const dispatch = useAppDispatch();
   const state = useAppSelector(selectActivity);
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
     dispatch(setInput({ value }));
   };
 
   const handleRot13Request = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
-    console.log(`Sending GET request on route http://localhost:8080/rot13/decode?message=${state.input}`);
+    console.log(
+      `Sending GET request on route http://localhost:8080/rot13/decode?message=${state.input}`
+    );
     axios
-    .get(`http://localhost:8080/rot13/decode?message=${state.input}`)
-    .then((response) => {
-      console.log("Response from server: ", response);
-      const value = "Rot13 result: " + response.data;
-      dispatch(setOutput({ value }));
-    })
-    .catch((err) => console.log("There was an error: " + err));
+      .get(`http://localhost:8080/rot13/decode?message=${state.input}`)
+      .then((response) => {
+        console.log("Response from server: ", response);
+        const value = "Rot13 result: " + response.data;
+        dispatch(setOutput({ value }));
+      })
+      .catch((err) => console.log("There was an error: " + err));
   };
-  
+
   const handleDec2OctRequest = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
-    console.log(`Sending GET request on route http://localhost:8080/dec2oct/convert?decString=${state.input}`);
-    axios.get(`http://localhost:8080/dec2oct/convert?decString=${state.input}`)
-    .then(response => {
-      console.log("Response from server: ", response);
-      const value = "Dec2Oct result: " + response.data;
-      dispatch(setOutput({ value }));
-    })
-  }
+    console.log(
+      `Sending GET request on route http://localhost:8080/dec2oct/convert?decString=${state.input}`
+    );
+    axios
+      .get(`http://localhost:8080/dec2oct/convert?decString=${state.input}`)
+      .then((response) => {
+        console.log("Response from server: ", response);
+        const value = "Dec2Oct result: " + response.data;
+        dispatch(setOutput({ value }));
+      });
+  };
+
+  const longestNonrepeatingSubstring = (str: string): string => {
+    let answer = "";
+    let candidate = "";
+    let seen = new Set();
+    str.split("").forEach((char, idx) => {
+        if (!seen.has(char)) {
+          seen.add(char);
+          candidate += char;
+          if (candidate.length > answer.length) {
+            answer = candidate;
+          }
+        } else {
+          seen = new Set();
+          seen.add(char);
+          candidate = char;
+        }
+    });
+    return answer;
+  };
 
   return (
     <React.Fragment>
@@ -47,12 +72,22 @@ const OnBoardActivity9_13_2021 = () => {
         <form>
           <label>Input Text:</label>
           <br></br>
-          <input onChange={handleChange}></input>
+          <input onChange={handleInputChange}></input>
           <br></br>
           <button onClick={handleRot13Request}>Java: Rot13</button>
           <button onClick={handleDec2OctRequest}>Java: Decimal2Octal</button>
           <br></br>
-          <button onClick={() => {}}>JavaScript: Decimal to Hex</button>
+          <button
+            onClick={(e: React.MouseEvent<HTMLButtonElement>) => {
+              e.preventDefault();
+              const value =
+                "Longest Non-repeating substring result is: " +
+                longestNonrepeatingSubstring(state.input);
+              dispatch(setOutput({ value }));
+            }}
+          >
+            JavaScript: Longest Nonrepeating Substring
+          </button>
           <br></br>
           <h1>{state.output}</h1>
           <br></br>
